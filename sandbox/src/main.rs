896908ebea87f84a6a9323abdf21ea8aeb92d061
devngugi <philemonngugi18@gmail.com>
2025-04-23 10:00:35 +0300

    borrowing and reference

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,3 +1,29 @@
 fn main() {
-    println!("Hello, world!");
+    let mut account: BankAccount = BankAccount {
+        balance: 150.55,
+        owner: "Alice".to_string(),
+    };
+
+    //immutable borrow to check balance
+    account.check_balance();
+
+        //mutable borrow
+    account.withdraw(10.1);
+
+
+}
+
+struct BankAccount {
+    balance: f64,
+    owner: String,
+}
+
+impl BankAccount {
+    fn withdraw(&mut self, amount: f64) {
+        println!("WIthdrawing {} from {} 's account", amount, self.owner);
+        self.balance -= amount;
+    }
+    fn check_balance(&self) {
+        println!("{}'s account balance is {}", self.owner, self.balance)
+    }
 }
